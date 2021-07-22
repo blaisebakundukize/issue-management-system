13c4a6127b3c840be5d5257b1279d1650542a766
Blaise Bakundukize <blaisebakundukize@gmail.com>
2021-07-22 09:52:05 +0200

    added roles and get all users

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -3,6 +3,7 @@ import authController from './auth.controller';
 
 const authRouter = express.Router();
 
+// Login user - running seeds for populating database is required because the register endpoint misses
 authRouter.post('/login', authController.login);
 
 export default authRouter;
--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -4,7 +4,10 @@ import { requiredToken, requireRoles } from '../../middleware/auth.middleware';
 
 const userRouter = express.Router();
 
+// Get all users - only admin allowed to get users
 userRouter.get('/', requiredToken, requireRoles(['admin']), userController.getAllUsers);
+
+// Get issues assigned to user
 userRouter.get('/issues/assigned', requiredToken, userController.getAssignedIssues);
 
 export default userRouter;
